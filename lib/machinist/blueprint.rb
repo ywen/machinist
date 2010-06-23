c162d564b319d65a873986163fc7e2fc71e06034
Pete Yandell <pete@notahat.com>
2010-06-23 11:20:57 +1000

    Got the pass_attributes_to_new strategy working.

--- lib/machinist/blueprint.rb
+++ lib/machinist/blueprint.rb
@@ -11,7 +11,7 @@ module Machinist
     def initialize(klass, options = {}, &block)
       @klass    = klass
       @parent   = options[:parent]
-      @strategy = options[:strategy] || NormalStrategy
+      @strategy = options[:strategy] || Strategies.default
       @block    = block
     end
 
